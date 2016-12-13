62885c0e210c9303f20d280e7f1d855bf51a55cd
chrigeler <christian@gueder.ch>
2016-12-13 20:38:10 +0100

    Update install-ltspi.sh
    Added remark "for Ubuntu 16.04".

--- install-ltspi.sh
+++ install-ltspi.sh
@@ -3,7 +3,7 @@
 # LTSPi installation script
 #
 # This script will install a Linux Terminal Server Project (LTSP) server for
-# Raspberry Pi 3 that are capable booting via PXE.
+# Raspberry Pi 3 that are capable booting via PXE on Ubuntu 16.04.
 #
 # Copyright (C) 2016 Simon Nussbaum <simon.nussbaum@linuxola.org
 # 
